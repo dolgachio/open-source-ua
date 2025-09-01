bfb51e56bd53169d4d1aba451945637fc52801dc
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-09-01 13:49:48 +0300

    docs/chore: add tags, update README

---
+++ src/constant/index.ts
@@ -0,0 +1,13 @@
+export {
+    ANGULAR_TAG,
+    REACT_TAG,
+    VUE_TAG,
+    JAVASCRIPT_TAG,
+    TYPESCRIPT_TAG,
+    NODEJS_TAG,
+    CSS_TAG,
+    HTML_TAG,
+    DOCS_TAG,
+    TIPS_TAG,
+    TOOLS_TAG
+} from './tag.constant';
---
+++ src/constant/tag.constant.ts
@@ -0,0 +1,12 @@
+export const ANGULAR_TAG = 'angular';
+export const REACT_TAG = 'react';
+export const VUE_TAG = 'vue';
+export const JAVASCRIPT_TAG = 'javascript';
+export const TYPESCRIPT_TAG = 'typescript';
+export const NODEJS_TAG = 'nodejs';
+export const CSS_TAG = 'css';
+export const HTML_TAG = 'html';
+
+export const DOCS_TAG = 'docs';
+export const TIPS_TAG = 'tips';
+export const TOOLS_TAG = 'tools';
