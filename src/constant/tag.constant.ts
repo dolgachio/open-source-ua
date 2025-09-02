a91c899a3d731b6518b106eb85c316a58d53f1b6
Stanislav Dolgachov <s.dolgachov@gmail.com>
2025-09-02 21:06:05 +0300

    docs: add starlight guide, started 02-post

--- src/constant/tag.constant.ts
+++ src/constant/tag.constant.ts
@@ -6,6 +6,7 @@ export const TYPESCRIPT_TAG = 'typescript';
 export const NODEJS_TAG = 'nodejs';
 export const CSS_TAG = 'css';
 export const HTML_TAG = 'html';
+export const ASTRO_TAG = 'astro';
 
 export const DOCS_TAG = 'docs';
 export const TRANSLATE_TAG = 'translate';
